9257e641ff8fc7ddc84f343c3dc4be257053c382
shubham-ghuge <shubhamghuge34@gmail.com>
2021-08-11 17:00:54 +0530

    changed api url

--- src/base.js
+++ src/base.js
@@ -1,2 +1,2 @@
-// export const API_URL = import.meta.env.VITE_BASE_URL;
-export const API_URL = "http://localhost:3002";
+export const API_URL = import.meta.env.VITE_BASE_URL;
+// export const API_URL = "http://localhost:3002";
